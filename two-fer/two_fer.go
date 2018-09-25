c3e9c4cc343398b918996dd3789621c5f81fb222
Obsidian <ObsidianRock@users.noreply.github.com>
2018-09-25 14:18:39 +0100

    two-fer

---
+++ two-fer/two_fer.go
@@ -0,0 +1,13 @@
+package twofer
+
+import "fmt"
+
+// ShareWith should have a comment documenting it.
+func ShareWith(name string) string {
+
+	if len(name) != 0 {
+		return fmt.Sprintf("One for %s, one for me.", name)
+	}
+
+	return "One for you, one for me."
+}
