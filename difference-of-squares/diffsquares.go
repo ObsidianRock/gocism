fe2f967d5e9f44091b6262c4b6d6dfedd0fc1a97
Obsidian <ObsidianRock@users.noreply.github.com>
2018-09-28 14:34:38 +0100

    luhn

--- difference-of-squares/diffsquares.go
+++ difference-of-squares/diffsquares.go
@@ -1,5 +1,6 @@
 package diffsquares
 
+//SquareOfSum finds the square sum of the fist N natural numbers
 func SquareOfSum(num int) int {
 
 	var count int
@@ -12,6 +13,7 @@ func SquareOfSum(num int) int {
 	return count * count
 }
 
+//SumOfSquares finds the sum of squares of the fist N natural numbers
 func SumOfSquares(num int) int {
 
 	var count int
@@ -22,6 +24,7 @@ func SumOfSquares(num int) int {
 	return count
 }
 
+//Difference Find the difference between the square of the sum and the sum of the squares of the first N natural numbers.
 func Difference(num int) int {
 
 	return SquareOfSum(num) - SumOfSquares(num)
---
+++ luhn/luhn.go
@@ -0,0 +1,52 @@
+package luhn
+
+import (
+	"strconv"
+	"strings"
+)
+
+//Valid given a number determine whether or not it is valid per the Luhn formula
+func Valid(text string) bool {
+
+	stp := strings.TrimSpace(text)
+
+	s := strings.Replace(stp, " ", "", -1)
+
+	if len(s) <= 1 {
+		return false
+	}
+
+	var numbers []int
+
+	for i := 0; i < len(s); i++ {
+		num, err := strconv.ParseInt(string(s[i]), 10, 64)
+		if err != nil {
+			return false
+		}
+		numbers = append(numbers, int(num))
+	}
+
+	return doubleSum(numbers)%10 == 0
+}
+
+func doubleSum(num []int) int {
+
+	var count int
+	for i := 0; i < len(num); i++ {
+		j := len(num) - i
+
+		if j%2 == 0 {
+
+			d := num[i] * 2
+
+			if d > 9 {
+				d -= 9
+			}
+			num[i] = d
+		}
+
+		count += num[i]
+	}
+
+	return count
+}
